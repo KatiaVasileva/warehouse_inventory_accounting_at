48d2c03d287201f20f690cdd4a699763710d74cf
katiavasileva <eka.vasileva@gmail.com>
2023-11-13 16:44:01 +0500

    Initial Commit

---
+++ src/main/java/ru/skypro/warehouse_inventory_accounting_at/WarehouseInventoryAccountingAtApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.warehouse_inventory_accounting_at;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseInventoryAccountingAtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseInventoryAccountingAtApplication.class, args);
+    }
+
+}
